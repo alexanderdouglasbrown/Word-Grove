4ce7f986a1a5ee7d1ccc81615dacd6322ce49636
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2019-12-26 17:16:09 -0800

    Minor changes to homepage and individual posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,9 +2,20 @@ import React from 'react'
 import Linkify from 'react-linkify'
 
 const Post = props => {
+    const { post, username, date } = props.postData
+
     return (
         <div className="card" style={{ padding: "1rem", marginTop: 0, marginBottom: "0.75rem", whiteSpace: "pre-wrap" }}>
-            <Linkify>{props.post}</Linkify>
+            <Linkify>{post}</Linkify>
+            <div style={{ marginTop: "0.5rem", width: "100%", backgroundColor: "lightgray", height: "2px", opacity: 0.2, borderRadius: "0.5rem" }}></div>
+            <div style={{ display: "flex", justifyContent: "space-between", marginTop: "0.5rem", fontSize: "0.7rem", color: "gray" }}>
+                <div>
+                    {`${username}, ${date}`}
+                </div>
+                <div>
+                    Comment
+                </div>
+            </div>
         </div>)
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState, useContext, useCallback } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
@@ -11,9 +11,9 @@ const Home = props => {
     const [userData] = useContext(UserContext)
     const [sayHi, setSayHi] = useState(null) // Check if connection to server established
 
-    const [posts, setPosts] = useState([])
+    const [postData, setPostData] = useState([])
 
-    const sendHello = () => {
+    const sendHello = useCallback(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/home/hi`)
             .then(() => {
                 setSayHi(true)
@@ -23,12 +23,11 @@ const Home = props => {
                 // If an error occurred, it is likely because my free Heroku dyno hasn't spun up yet, so keep spamming until it does
                 setTimeout(() => { sendHello() }, 1000)
             })
-    }
+    }, [])
 
     useEffect(() => {
         sendHello()
-        // eslint-disable-next-line
-    }, [])
+    }, [sendHello])
 
     useEffect(() => {
         if (sayHi) {
@@ -38,10 +37,11 @@ const Home = props => {
 
     const loadPosts = () => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/home/posts`)
-        .then(res => {
-            setPosts(res.data)
-        })
-        .catch(() => toast.error("Sorry, something went wrong"))
+            .then(res => {
+                setPostData(res.data)
+                console.log(res.data)
+            })
+            .catch(() => toast.error("Sorry, something went wrong"))
     }
 
     if (sayHi === null)
@@ -56,9 +56,9 @@ const Home = props => {
             :
             <>
                 {userData.isLoggedIn ?
-                            <PostBox loadPosts={loadPosts} />
+                    <PostBox loadPosts={loadPosts} />
                     : null}
-                    {posts.map(post=><Post key={post.id} post={post.post} />)}
+                {postData.map(postData => <Post key={postData.id} postData={postData} />)}
             </>
         }
     </div>)
