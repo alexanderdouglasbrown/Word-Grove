bf1bd73f833467b750e7d0034baba1d2f1c4b0ef
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2019-12-27 21:23:10 -0800

    Don't expand post when chasing a link

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,9 +6,14 @@ import '../components/css/LinkButton.css'
 const Post = props => {
     const { post, username, date, id } = props.postData
 
+    const expandPost = e => {
+        if (e.target && e.target.tagName && e.target.tagName.toLowerCase() !== "a")
+            props.expandPost(id)
+    }
+
     return (
         <div className="card" style={{ margin: "1rem auto" }}>
-            <div className="card-content" style={{ whiteSpace: "pre-wrap", cursor: "pointer" }} onClick={() => props.expandPost(id)}>
+            <div className="card-content" style={{ whiteSpace: "pre-wrap", cursor: "pointer" }} onClick={e => expandPost(e)}>
                 <Linkify>{post}</Linkify>
             </div>
             <div className="card-footer" style={{ justifyContent: "space-between", fontSize: "0.7rem", color: "gray", padding: "1rem" }}>
