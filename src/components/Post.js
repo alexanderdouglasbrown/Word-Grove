7e284ce74793c5a09d36a61bbd5fbe1cb96eb24c
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2019-12-30 13:50:19 -0800

    Reworking Posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,32 +1,157 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback, useContext } from 'react'
 import Linkify from 'react-linkify'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+import UserContext from '../UserContext'
 
 import '../components/css/LinkButton.css'
 
 const Post = props => {
-    const { post, username, id } = props.postData
+    const maxCharacters = 512
 
-    const expandPost = e => {
+    const [userData] = useContext(UserContext)
+
+    const { postID, isExpanded } = props
+    const [postData, setPostData] = useState(null)
+
+    const [isEditMode, setIsEditMode] = useState(false)
+    const [editInput, setEditInput] = useState("")
+    const [characterCounter, setCharacterCounter] = useState(maxCharacters)
+
+    const expandPostClicked = e => {
         if (e.target && e.target.tagName && e.target.tagName.toLowerCase() !== "a")
-            props.expandPost(id)
+            props.expandPost(postID)
     }
 
-    return (
-        <div className="card" style={{ margin: "1rem auto" }}>
-            <div className="card-content" style={{ whiteSpace: "pre-wrap", cursor: "pointer" }} onClick={e => expandPost(e)}>
-                <Linkify>{post}</Linkify>
-            </div>
-            <div className="card-footer" style={{ justifyContent: "space-between", fontSize: "0.7rem", color: "gray", padding: "1rem" }}>
-                <div>
+    const refreshPost = useCallback(() => {
+        axios.get(`${process.env.REACT_APP_API_URL}/api/post`,
+            { params: { ID: postID } })
+            .then(res => {
+                setPostData(res.data)
+            })
+            .catch(err => {
+                if (err && err.response && err.response.data && err.response.data.error)
+                    toast.error(err.response.data.error)
+                else
+                    toast.error("Sorry, an error occured")
+            })
+    }, [postID])
+
+    const handleInput = e => {
+        let text = e.target.value
+
+        if (text.length > maxCharacters)
+            text = text.substr(0, maxCharacters)
+
+        setCharacterCounter(maxCharacters - text.length)
+        setEditInput(text)
+    }
+
+    const startEdit = () => {
+        const text = postData && postData.post ? postData.post : ""
+        setEditInput(text)
+        setIsEditMode(true)
+        setCharacterCounter(maxCharacters - text.length)
+    }
+
+    const cancelEdit = () => {
+        setEditInput("")
+        setIsEditMode(false)
+        setCharacterCounter(maxCharacters)
+    }
+
+    useEffect(() => {
+        if (!postData)
+            refreshPost()
+    }, [refreshPost, postData])
+
+    const deletePost = () => {
+        if (window.confirm("Are you sure you would like to delete this post?")) {
+            axios.delete(`${process.env.REACT_APP_API_URL}/api/post`,
+                { data: { ID: postID }, headers: { Authorization: userData.token } })
+                .then(() => {
+                    // TODO return nothing I guess
+                })
+                .catch(err => {
+                    if (err && err.response && err.response.data && err.response.data.error)
+                        toast.error(err.response.data.error)
+                    else
+                        toast.error("Sorry, an error occured")
+                })
+        }
+    }
+
+    const saveEdit = () => {
+        axios.patch(`${process.env.REACT_APP_API_URL}/api/post`,
+            { ID: postID, Post: editInput }, { headers: { Authorization: userData.token } })
+            .then(() => {
+                refreshPost()
+                cancelEdit()
+                refreshPost()
+            })
+            .catch(err => {
+                if (err && err.response && err.response.data && err.response.data.error)
+                    toast.error(err.response.data.error)
+                else
+                    toast.error("Sorry, an error occured")
+            })
+    }
+
+
+    return <>
+        {postData ?
+            <div className="card" style={{ margin: "1rem auto" }}>
+                {isExpanded ?
+                    <div className="card-content">
+                        {isEditMode ?
+                            <>
+                                <div className="LinkButton-Danger" onClick={deletePost} style={{ position: "absolute", top: "0.25rem", right: "0.25rem", fontSize: "0.7rem" }}>Delete</div>
+                                <textarea onChange={handleInput} value={editInput} className="textarea" />
+                                <div style={{ float: "right", color: "lightgray" }} >Remaining characters: {characterCounter}</div>
+                            </>
+                            :
+                            <Linkify>{postData.post}</Linkify>
+                        }
+                    </div>
+                    :
+                    <div className="card-content" style={{ whiteSpace: "pre-wrap", cursor: "pointer" }} onClick={expandPostClicked}>
+                        <Linkify>{postData.post}</Linkify>
+                    </div>
+                }
+
+                <div className="card-footer" style={{ justifyContent: "space-between", fontSize: "0.7rem", color: "gray", padding: "1rem" }}>
+                    {(userData.userID === postData.userID || userData.access === "Admin") &&
+                        <>
+                            {isEditMode ?
+                                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                                    <div className="LinkButton" onClick={cancelEdit}>Cancel</div>
+                                    <div style={{ width: "0.5rem" }}></div>
+                                    <div className="LinkButton" onClick={saveEdit} style={{ fontWeight: 700 }}>Save</div>
+                                </div>
+                                :
+                                <div className="LinkButton" onClick={startEdit}>Edit</div>
+                            }
+                        </>
+                    }
+                    {/* <div>
                     {!props.hidePostedBy && <>
                         <div style={{ display: "inline-block" }}>Posted by&nbsp;</div>
                         <div style={{ display: "inline-block" }} className="LinkButton"><Link to={`/p/${username}`}>{`${username}`}</Link></div>
                     </>}
                 </div>
-                <div className="LinkButton">Like</div>
+                <div className="LinkButton">Like</div> */}
+                </div>
             </div>
-        </div>)
+            :
+            <div className="card" style={{ margin: "1rem auto" }}>
+                <div className="card-content">
+                    <progress className="progress is-small is-light" max="100"></progress>
+                </div>
+            </div>
+        }
+    </>
 }
 
 export default Post
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,34 +12,29 @@ const Home = props => {
     const [userData] = useContext(UserContext)
     const [sayHi, setSayHi] = useState(null) // Check if connection to server established
 
-    const [postData, setPostData] = useState([])
+    const [postIDs, setPostIDs] = useState(null)
 
     const [isPostModalVisible, setIsPostModalVisible] = useState(false)
     const [selectedPostID, setSelectedPostID] = useState(null)
 
-    const getPostData = useRef()
+    const getPostIDs = useRef()
     const isFetching = useRef(false)
-    getPostData.current = () => { return postData }
+    getPostIDs.current = () => { return postIDs }
 
-    const handleScroll = useCallback(e => {
-        if (!isFetching.current && window.innerHeight + document.documentElement.scrollTop + 200 >= document.documentElement.offsetHeight)
-            getMorePosts()
-    }, [])
-
-    const refreshPosts = () => {
+    const refreshPosts = useCallback(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/home/posts`,
             { params: { LastID: null } })
-            .then(res => setPostData(res.data))
+            .then(res => setPostIDs(res.data))
             .catch(() => toast.error("Sorry, something went wrong"))
-    }
+    }, [])
 
-    const getMorePosts = () => {
-        const posts = getPostData.current()
+    const getMorePosts = useCallback(() => {
+        const posts = getPostIDs.current()
 
         if (!posts || posts.length < 1)
             return
 
-        const lastID = posts[posts.length - 1].id
+        const lastID = posts[posts.length - 1]
 
         if (lastID <= 1)
             return
@@ -47,10 +42,10 @@ const Home = props => {
         isFetching.current = true
         axios.get(`${process.env.REACT_APP_API_URL}/api/home/posts`,
             { params: { lastID } })
-            .then(res => setPostData([...posts, ...res.data]))
+            .then(res => setPostIDs([...postIDs, ...res.data]))
             .catch(() => toast.error("Sorry, something went wrong"))
             .finally(() => isFetching.current = false)
-    }
+    }, [postIDs])
 
     const closePostModal = () => {
         setIsPostModalVisible(false)
@@ -76,42 +71,46 @@ const Home = props => {
             })
     }, [])
 
+    const handleScroll = useCallback(e => {
+        if (!isFetching.current && window.innerHeight + document.documentElement.scrollTop + 200 >= document.documentElement.offsetHeight)
+            getMorePosts()
+    }, [getMorePosts])
+
     useEffect(() => {
         window.addEventListener('scroll', handleScroll)
         return () => window.removeEventListener('scroll', handleScroll)
     }, [handleScroll])
 
     useEffect(() => {
-        sendHello()
-    }, [sendHello])
-
-    useEffect(() => {
-        if (sayHi) {
+        if (!sayHi) {
+            sendHello()
+        } else if (!postIDs) {
             refreshPosts()
         }
-    }, [sayHi])
+    }, [sendHello, sayHi, refreshPosts, postIDs])
 
     useEffect(() => { // Trigger infinite scroll if non enough height for a scrollbar
-        if (postData && postData.length > 0 && window.innerHeight > document.documentElement.offsetHeight)
+        if (postIDs && postIDs.length > 0 && window.innerHeight > document.documentElement.offsetHeight)
             handleScroll()
-    }, [postData, handleScroll])
+    }, [postIDs, handleScroll])
 
     return <>
         {sayHi ?
             <>
                 <div className="container">
                     <>
-                        {userData.isLoggedIn ?
+                        {userData.isLoggedIn &&
                             <PostBox refreshPosts={refreshPosts} />
-                            : null}
-                        {postData.map(postData => <Post key={postData.id} postData={postData} expandPost={openPostModal} />)}
+                        }
+                        {postIDs &&
+                            postIDs.map(postID => <Post key={postID} postID={postID} expandPost={openPostModal} />)
+                        }
                     </>
                 </div>
                 <PostModal
                     isOpen={isPostModalVisible}
                     closeModal={closePostModal}
                     postID={selectedPostID}
-                    refresh={refreshPosts}
                 />
             </>
             :
