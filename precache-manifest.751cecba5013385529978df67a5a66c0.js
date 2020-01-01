e13964b70a3127535b7256925166ba788bca4ac5
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2020-01-01 11:31:06 -0800

    Updates

--- precache-manifest.751cecba5013385529978df67a5a66c0.js
+++ precache-manifest.751cecba5013385529978df67a5a66c0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f85e65d697a558c24000cfb9d755444b",
+    "revision": "691536c819f65898d1d23000ea7ecf54",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.9e2a7eb9.chunk.css"
   },
   {
-    "revision": "75189929ffdf558102c9",
+    "revision": "5ed774990cdc35c9738c",
     "url": "/static/css/main.0a40345a.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.015521b5.chunk.js.LICENSE"
   },
   {
-    "revision": "75189929ffdf558102c9",
-    "url": "/static/js/main.bba42feb.chunk.js"
+    "revision": "5ed774990cdc35c9738c",
+    "url": "/static/js/main.6e7e88db.chunk.js"
   },
   {
     "revision": "c41e0be6f1145052def7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f1b7481d4eb47629c772bf1b7ee9b23.js"
+  "/precache-manifest.751cecba5013385529978df67a5a66c0.js"
 );
 
 self.addEventListener('message', (event) => {
