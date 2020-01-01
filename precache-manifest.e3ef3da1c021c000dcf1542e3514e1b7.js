33368d30f7f4db70146e9932ab1ff63660d993f6
alexanderdouglasbrown <alexanderdouglasbrown@gmail.com>
2020-01-01 08:34:08 -0800

    Updates

---
+++ precache-manifest.e3ef3da1c021c000dcf1542e3514e1b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979bf71fc26dc068520ac2a8a0bacecc",
+    "url": "/Word-Hole/index.html"
+  },
+  {
+    "revision": "9cf7d215506f168706cd",
+    "url": "/Word-Hole/static/css/2.9e2a7eb9.chunk.css"
+  },
+  {
+    "revision": "fc31e51539989d0da4fb",
+    "url": "/Word-Hole/static/css/main.0a40345a.chunk.css"
+  },
+  {
+    "revision": "9cf7d215506f168706cd",
+    "url": "/Word-Hole/static/js/2.80f21c2b.chunk.js"
+  },
+  {
+    "revision": "0b4ff06a24fb6f4296fad408412d0a41",
+    "url": "/Word-Hole/static/js/2.80f21c2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc31e51539989d0da4fb",
+    "url": "/Word-Hole/static/js/main.aae0a899.chunk.js"
+  },
+  {
+    "revision": "1481cceb492a393279df",
+    "url": "/Word-Hole/static/js/runtime-main.0569590d.js"
+  }
+]);
